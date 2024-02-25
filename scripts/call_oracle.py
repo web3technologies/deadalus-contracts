fa4b83265d222f631028f9d318f780fb38378bf4
= <zach@web3technologies.io>
2024-02-25 08:22:05 -0700

    add main to oracle

--- scripts/call_oracle.py
+++ scripts/call_oracle.py
@@ -4,8 +4,13 @@ import argparse
 from deploy_modules import update_oracle
 
 
-parser = argparse.ArgumentParser(description='Script to deploy smart contracts.')
-parser.add_argument('--deploy-env', dest='deploy_env', type=str, help='Deployment environment (e.g., dev, int, prod)')
-parser.add_argument('--contract-address', dest="contract_address", type=str, help="Contract address of the oracle")
-args = parser.parse_args()
-asyncio.run(update_oracle(args.deploy_env, contract_address=args.contract_address))
+def main():
+    parser = argparse.ArgumentParser(description='Script to deploy smart contracts.')
+    parser.add_argument('--deploy-env', dest='deploy_env', type=str, help='Deployment environment (e.g., dev, int, prod)')
+    parser.add_argument('--contract-address', dest="contract_address", type=str, help="Contract address of the oracle")
+    args = parser.parse_args()
+    asyncio.run(update_oracle(args.deploy_env, contract_address=args.contract_address))
+
+
+if __name__ == "__main__":
+    main()
